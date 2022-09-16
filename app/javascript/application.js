596f4bf52780e2cb42b3893f0284ebb43beb3ba4
Jonathan Ferreira <jonathan.ferreira@pitzi.com>
2022-09-15 21:01:58 -0300

    aula 4 - Habilitando turbo drive

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,11 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 
-Turbo.session.drive = false
+// Habilita turbo drive.
+// Por padrão já vem habilitado
+// Turbo.session.drive = false
+
+
+// seta um delay para exibir um feedback visual no navegador
+// por padrão só é exibido quando a requisição demora mais de 500ms
+Turbo.setProgressBarDelay(1)
